fe88852c55f5882bff681a1faf0e6ed5b794d380
RonanCamargo <ronan.cam@gmail.com>
2022-06-18 14:14:03 -0300

    Clean up

--- src/main/scala/ronancamargo/csv/CsvMain.scala
+++ src/main/scala/ronancamargo/csv/CsvMain.scala
@@ -1,16 +1,11 @@
 package ronancamargo.csv
 
 import ronancamargo.csv.data.Employee
-import shapeless.Generic
 
 import scala.util.chaining._
 
 object CsvMain extends App {
 
-//  implicit val employeeEncoder: CsvEncoder[Employee] = new CsvEncoder[Employee] {
-//    override def encode(a: Employee): List[String] = List(a.name, a.number.toString, if (a.manager) "yes" else "no")
-//  }
-//
   def writeCsv[A](values: List[A])(implicit encoder: CsvEncoder[A]): String =
     values.map(v => encoder.encode(v).mkString(",")).mkString("\n")
 
@@ -21,7 +16,4 @@ object CsvMain extends App {
   )
 
   writeCsv(employees).tap(println)
-
-  val employeeGen = Generic[Employee]
-
 }
